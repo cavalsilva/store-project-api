83a73ff438915e3ed411a2e9797089b052a2384c
Ricardo <ricardocavalcantesilva@gmail.com>
2020-02-03 22:22:50 -0300

    Variável req.id no middleware e comentários

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const server = express();
 
 server.use(express.json());
 
+// Middleware que controle a quantidade de requisições feitas
 function logRequests(req, res, next) {
   console.count("Número de requisições");
   return next();
 }
 
-server.use(logRequests);
-
-const projects = [];
-
+// Middleware que verifica se o projeto existe
 function checkProjectExists(req, res, next) {
   const { id } = req.params;
   const project = projects.find(p => p.id == id);
@@ -20,10 +18,17 @@ function checkProjectExists(req, res, next) {
     return res.status(400).json({ error: 'Project not found' });
   }
 
+  req.id = id;
+
   return next();
 
 }
 
+server.use(logRequests);
+
+const projects = [];
+
+// Incluir um projeto
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
 
@@ -38,35 +43,36 @@ server.post('/projects', (req, res) => {
   return res.json(projects);
 });
 
+// Incluir uma tarefa
 server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
-  const { id } = req.params;
   const { title } = req.body;
 
-  const project = projects.find(p => p.id == id);
+  const project = projects.find(p => p.id == req.id);
 
   project.tasks.push(title);
 
   return res.json(projects);
 });
 
+// Recuperar todos os projetos
 server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
+// Atualizar título de um projeto
 server.put('/projects/:id', checkProjectExists, (req, res) => {
-  const { id } = req.params;
   const { title } = req.body;
 
-  const project = projects.find(p => p.id == id);
+  const project = projects.find(p => p.id == req.id);
 
   project.title = title;
 
   return res.json(projects);
 });
 
+// Excluir um projeto
 server.delete('/projects/:id', checkProjectExists, (req, res) => {
-  const { id } = req.params;
-  const project = projects.find(p => p.id == id);
+  const project = projects.find(p => p.id == req.id);
 
   projects.splice(project);
 
